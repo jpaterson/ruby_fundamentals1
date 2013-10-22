cbd9cf9aebc3e21822facff5f4d49fe1dd673dd2
Johnny <john@thebiggainer.com>
2013-10-22 14:21:02 -0400

    Exercise 3

---
+++ exercise3.rb
@@ -0,0 +1,10 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "Your name is therefore #{name}."
+sleep 1
+
+puts "How old are you?"
+age = gets.chomp.to_i
+
+puts "You are #{age} years old."
